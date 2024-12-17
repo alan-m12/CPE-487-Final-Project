07cf20f59b94b6fc1ca13eda6d8eaa9ce0eb3725
alan-m12 <63511002+alan-m12@users.noreply.github.com>
2024-12-16 19:24:11 -0500

    Update roulette.vhd

--- roulette.vhd
+++ roulette.vhd
@@ -2,34 +2,37 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity Roulette is
-   port(
-      CLK100MHZ : in std_logic;
-      SW : in std_logic_vector(15 downto 0);
-      BTNC : in std_logic;
-      SEGMENTS : out std_logic_vector(7 downto 0);
-      AN : out std_logic_vector(7 downto 0)
-   );
+    port(
+        CLK100MHZ : in std_logic;
+        SW : in std_logic_vector(15 downto 0);
+        BTN0 : in std_logic;
+        BTNU : in std_logic;  -- New button for mode 2
+        SEGMENTS : out std_logic_vector(7 downto 0);
+        AN : out std_logic_vector(7 downto 0);
+        LED : out std_logic_vector(1 downto 0)
+    );
 end entity Roulette;
 
 architecture Behavioral of Roulette is
     component my_fsm is
-        port ( 
+        port (
             SWITCHES : in std_logic_vector(15 downto 0);
             BTN0 : in std_logic;
+            BTNU : in std_logic;
             CLK : in std_logic;
             Z : out std_logic;
             START : out std_logic;
             BOMB_LOCATION : out std_logic_vector(3 downto 0);
-            CURRENT_PLAYER : out std_logic
+            CURRENT_PLAYER : out std_logic;
+            LED_STATE : out std_logic_vector(1 downto 0)
         );
     end component;
 
     component BC_DEC is
-        port ( 
-            CLK : in std_logic;
-            Z : in std_logic;
-            START : in std_logic;
-            DISP_EN : out std_logic_vector(7 downto 0);
+        port (    
+            CLK, Z : in std_logic;
+            START : in std_logic; 
+            DISP_EN : out std_logic_vector(7 downto 0);  
             SEGMENTS : out std_logic_vector(7 downto 0)
         );
     end component;
@@ -42,12 +45,14 @@ architecture Behavioral of Roulette is
 begin
     FSM_INST : my_fsm port map (
         SWITCHES => SW,
-        BTN0 => BTNC,
+        BTN0 => BTN0,
+        BTNU => BTNU,
         CLK => CLK100MHZ,
         Z => Z,
         START => START,
         BOMB_LOCATION => BOMB_LOCATION,
-        CURRENT_PLAYER => CURRENT_PLAYER
+        CURRENT_PLAYER => CURRENT_PLAYER,
+        LED_STATE => LED
     );
 
     BC_DEC_INST : BC_DEC port map (
